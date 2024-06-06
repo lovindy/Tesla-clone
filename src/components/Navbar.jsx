458b992d873e36db842627c9cbebba046442e172
AnB School 0012 <anbschool0012@AnBs-Mac-mini-77.local>
2024-06-06 14:43:30 +0700

    animation on the menu list.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,8 +60,7 @@ function Navbar() {
       <div
         className={`mx-auto relative z-[100] py-4 px-8 flex items-center justify-between ${
           menuOpen ? "bg-gray-100" : ""
-        }`}
-      >
+        }`}>
         <a href="#">
           <Tesla />
         </a>
@@ -80,20 +79,17 @@ function Navbar() {
         <ul
           className={`lg:flex font-medium text-sm tracking-wider ${
             menuOpen ? "" : "hidden"
-          }`}
-        >
+          }`}>
           {/* Your existing navigation links */}
           {/* Vehicles */}
           <motion.li
             className="relative group"
-            onMouseEnter={() => handleMouseEnter("vehicles")}
-          >
+            onMouseEnter={() => handleMouseEnter("vehicles")}>
             <a
               href="#vehicles"
               className={`hover:bg-[#f2f2f2] duration-500 px-5 py-[8px] rounded-md ${
                 activeCategory === "vehicles" ? "bg-[#f2f2f2]" : ""
-              }`}
-            >
+              }`}>
               Vehicles
             </a>
           </motion.li>
@@ -101,14 +97,12 @@ function Navbar() {
           {/* Energy */}
           <motion.li
             className="relative group"
-            onMouseEnter={() => handleMouseEnter("energy")}
-          >
+            onMouseEnter={() => handleMouseEnter("energy")}>
             <a
               href="#energy"
               className={`hover:bg-[#f2f2f2] duration-500 px-5 py-[8px] rounded-md ${
                 activeCategory === "energy" ? "bg-[#f2f2f2]" : ""
-              }`}
-            >
+              }`}>
               Energy
             </a>
           </motion.li>
@@ -116,14 +110,12 @@ function Navbar() {
           {/* Charging */}
           <motion.li
             className="relative group"
-            onMouseEnter={() => handleMouseEnter("charging")}
-          >
+            onMouseEnter={() => handleMouseEnter("charging")}>
             <a
               href="#charging"
               className={`hover:bg-[#f2f2f2] duration-500 px-5 py-[8px] rounded-md ${
                 activeCategory === "charging" ? "bg-[#f2f2f2]" : ""
-              }`}
-            >
+              }`}>
               Charging
             </a>
           </motion.li>
@@ -131,14 +123,12 @@ function Navbar() {
           {/* Discover */}
           <motion.li
             className="relative group"
-            onMouseEnter={() => handleMouseEnter("discover")}
-          >
+            onMouseEnter={() => handleMouseEnter("discover")}>
             <a
               href="#discover"
               className={`hover:bg-[#f2f2f2] duration-500 px-5 py-[8px] rounded-md ${
                 activeCategory === "discover" ? "bg-[#f2f2f2]" : ""
-              }`}
-            >
+              }`}>
               Discover
             </a>
           </motion.li>
@@ -146,14 +136,12 @@ function Navbar() {
           {/* Shop */}
           <motion.li
             className="relative group"
-            onMouseEnter={() => handleMouseEnter("shop")}
-          >
+            onMouseEnter={() => handleMouseEnter("shop")}>
             <a
               href="#shop"
               className={`hover:bg-[#f2f2f2] duration-500 px-5 py-[8px] rounded-md ${
                 activeCategory === "shop" ? "bg-[#f2f2f2]" : ""
-              }`}
-            >
+              }`}>
               Shop
             </a>
           </motion.li>
@@ -197,8 +185,7 @@ function Navbar() {
             exit="hidden"
             variants={menuVariants}
             transition={{ duration: 0.3 }}
-            className="bg-white top-0 fixed text-[20px] z-[101] p-4 w-full h-screen flex flex-col items-end"
-          >
+            className="bg-white top-0 fixed text-[20px] z-[101] p-4 w-full h-screen flex flex-col items-end">
             <button
               className="m-4 px-[1px] duration-300 border-none rounded-md hover:bg-[#f2f2f2]"
               onClick={closeMenu} // Close new menu on click
@@ -209,8 +196,7 @@ function Navbar() {
               {menuItems.map((item, index) => (
                 <li
                   key={index}
-                  className="hover:bg-[#f2f2f2] duration-300 rounded-lg px-2 py-4 flex justify-between items-center"
-                >
+                  className="hover:bg-[#f2f2f2] duration-300 rounded-lg px-2 py-4 flex justify-between items-center">
                   <a href="#">{item.label}</a>
                   {item.showChevron && <ChevronRightIcon />}
                 </li>
@@ -240,8 +226,7 @@ function Navbar() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.6, ease: [0.76, 0, 0.24, 1] }}
-            className="fixed w-full h-screen top-0 z-[98] bg-black bg-opacity-10 backdrop-filter backdrop-blur-sm"
-          ></motion.div>
+            className="fixed w-full h-screen top-0 z-[98] bg-black bg-opacity-10 backdrop-filter backdrop-blur-sm"></motion.div>
         )}
       </AnimatePresence>
     </div>
